0662d4b9223ca3c04c0861c614b8b28e7e4d572a
Hallcoder <hallcoder25@gmail.com>
2023-01-08 14:23:51 +0200

    allow any origin

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(function(req,res,next){
-    cors({origin:req.headers.origin})
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    cors({origin:"*"})
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'PUT,GET,POST')
     res.header('Access-Control-Allow-Headers', 'X-Request-With,Origin,Accept,Content-Type,X-HTTP-Method-Override')
     res.header('Access-Control-Allow-Credentials', true)
